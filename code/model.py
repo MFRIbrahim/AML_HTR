a2257d6372f9b1d16e498bffd4aa89428b9eef2a
Armand Rousselot <armand.r@live.de>
2019-08-07 19:39:32 +0200

    added training loop and word encoding, fixed minor bug in Decoder function

--- code/model.py
+++ code/model.py
@@ -8,31 +8,34 @@ CHAR_LIST = list(" !\"#&'()*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefgh
 CHAR_DICT = {}
 for i in range(len(CHAR_LIST)):
     CHAR_DICT[i] = CHAR_LIST[i]
-    
+INV_CHAR_DICT = {v: k for k, v in CHAR_DICT.items()}
+
 def Decoder(matrix):
     # matrix with shape (seq_len, batch_size, num_of_characters) --> (32,50,80)
-    C = np.argmax(matrix, axis=2).T  
+    C = np.argmax(matrix, axis=2).T
     output = []
-    for i in range(C.shape[0]):
+    #iterate over dim 1 first, since those are the batches
+    for i in range(C.shape[1]):
         sub = []
-        for j in range(C.shape[1]):
-            sub.append(CHAR_DICT[C[i][j]])
+        #iterate over the sequence
+        for j in range(C.shape[0]):
+            sub.append(CHAR_DICT[C[j][i]])
         output.append(sub)
-    return output    
-    
-    
+    return output
+
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        
-        
+
+
         # ---CNN layers---
         self.cnn_layers = nn.ModuleList()
-        
+
         conv_kernel = [5, 5, 3, 3, 3]
         channels = [1, 32, 64, 128, 128, 256]
         pool_kernel_stride = [(2,2), (2,2), (2,1), (2,1), (2,1)]
-        
+
         for i in range(len(conv_kernel)):
             if conv_kernel[i] == 5:
                 self.cnn_layers.append(nn.Conv2d(in_channels=channels[i], out_channels=channels[i+1], kernel_size=conv_kernel[i], stride=1, padding=2))
@@ -41,14 +44,14 @@ class Net(nn.Module):
             self.cnn_layers.append(nn.BatchNorm2d(num_features=channels[i+1]))
             self.cnn_layers.append(nn.ReLU())
             self.cnn_layers.append(nn.MaxPool2d(kernel_size=pool_kernel_stride[i], stride=pool_kernel_stride[i], padding=0))
-        
-        # ---LSTM---    
+
+        # ---LSTM---
         self.lstm = nn.LSTM(input_size=256, hidden_size=256, num_layers=2, batch_first=True, bidirectional=True)
-        
+
         #---last CNN layer---
         self.cnn = nn.Conv2d(in_channels=512, out_channels=80, kernel_size=1, stride=1, padding=0)
-    
-    
+
+
     def forward(self, x):
         # pass through CNN layers
         for layer in self.cnn_layers:
@@ -62,36 +65,51 @@ class Net(nn.Module):
         # transformation for last CNN layer
         x = torch.unsqueeze(x,2)
         x = x.permute(0,3,2,1)
-        # pass through last CNN layer 
+        # pass through last CNN layer
         x = self.cnn(x)
         # transform for CTC_loss calc and text decoding
         x = torch.squeeze(x)
         x = x.permute(2,0,1)
         return x
 
+def encodeWord(Y):
+    new_Y = []
+    for w in Y:
+        out = []
+        for letter in w:
+            out.append(INV_CHAR_DICT[letter])
+        new_Y.append(out)
+    return np.asarray(Y)
 
-def training():
-    net = Net()
-    learning_rate = 0.001
-    loss = nn.CTCLoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+def training(model, dataloader, learning_rate=0.001, verbose = True):
+    loss_fct = nn.CTCLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    #iterate over batches
+    for (batch_id, (X, Y)) in enumerate(dataloader):
+        Y = encodeWord(Y)
+        optimizer.zero_grad()
+        model_out = model(X)
+        ctc_input = F.log_softmax(model_out)
+        input_lengths = torch.full(size=(len(X),), fill_value=model_out.shape[0], dtype=torch.long)
+        #TODO: Check axis
+        ctc_target = np.concatenate(Y, axis = 0)
+        target_lengths = []
+        for w in Y:
+            target_lengths.append(len(Y))
+        loss = loss_fct(ctc_input, ctc_target, input_lengths, target_lengths)
+        loss.backward()
+        optimizer.step()
+        if verbose:
+            print("Processed Batch {}/{}".format(batch_id, len(dataloader)))
+            print("Loss: {}".format(loss))
 
 
-net = Net()
-
-a = torch.rand(50, 1, 32, 128)
-
-
-b = net(a)
-
-ctc_input = F.log_softmax(b)
-ctc_target = torch.randint(low=1, high=80, size=(50, 32), dtype=torch.long)
-input_lengths = torch.full(size=(50,), fill_value=32, dtype=torch.long)
-target_lengths = torch.randint(low=1, high=32, size=(50,), dtype=torch.long)
-ctc_loss = nn.CTCLoss()
-loss = ctc_loss(ctc_input, ctc_target, input_lengths, target_lengths)
-loss.backward()
-
-print(loss)
-c = b.data.numpy()
-print(Decoder(c))
+if __name__=="__main__":
+    model = Net()
+    n_epochs = 10
+    #TODO: Define Dataloader properly (either via pytorch or the github example)
+    dataloader = None
+    for epoch in range(n_epochs):
+        print("Training Epoch "+ str(epoch+1))
+        #training(model, dataloader)
+        print("Not training due to missing dataloader implementation")
