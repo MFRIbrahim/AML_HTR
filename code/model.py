34bdb9adf265fb0c78475b767d5d5b4f50b78d4c
Armand Rousselot <armand.r@live.de>
2019-08-10 21:09:30 +0200

    added data conversion and started training

--- code/model.py
+++ code/model.py
@@ -3,7 +3,14 @@ import torch
 from torch import nn as nn
 from torch.nn import functional as F
 from PIL import Image as PImage
+import cv2
 import torchvision
+import math
+
+if torch.cuda.is_available():
+    device = 'cuda'
+else:
+    device = 'cpu'
 
 
 CHAR_LIST = list(" !\"#&'()*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
@@ -79,25 +86,28 @@ def encodeWord(Y):
     for w in Y:
         out = []
         for letter in w:
-            out.append(INV_CHAR_DICT[letter])
-        new_Y.append(out)
-    return np.asarray(Y)
+            out.append(int(INV_CHAR_DICT[letter]))
+        new_Y.append(np.asarray(out))
+    return new_Y
 
 def training(model, dataloader, learning_rate=0.001, verbose = True):
-    loss_fct = nn.CTCLoss()
+    loss_fct = nn.CTCLoss().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     #iterate over batches
     for (batch_id, (X, Y)) in enumerate(dataloader):
         Y = encodeWord(Y)
+        X = X.to(device)
         optimizer.zero_grad()
         model_out = model(X)
-        ctc_input = F.log_softmax(model_out)
-        input_lengths = torch.full(size=(len(X),), fill_value=model_out.shape[0], dtype=torch.long)
+        ctc_input = F.log_softmax(model_out).to(device)
+        input_lengths = torch.full(size=(len(X),), fill_value=model_out.shape[0], dtype=torch.long).to(device)
         #TODO: Check axis
         ctc_target = np.concatenate(Y, axis = 0)
         target_lengths = []
         for w in Y:
-            target_lengths.append(len(Y))
+            target_lengths.append(len(w))
+        target_lengths = torch.Tensor(target_lengths).to(device).type(torch.int32)
+        ctc_target = torch.Tensor(ctc_target).to(device).type(torch.int32)
         loss = loss_fct(ctc_input, ctc_target, input_lengths, target_lengths)
         loss.backward()
         optimizer.step()
@@ -106,18 +116,17 @@ def training(model, dataloader, learning_rate=0.001, verbose = True):
             print("Loss: {}".format(loss))
 
 
-def data_loader(words_file, data_dir, batch_size):
-    #TODO: scale all the inputs to 32x128
+def data_loader(words_file, data_dir, batch_size, image_size):
     # words_file: absolute path of words.txt
     # data_dir: absolute path of directory that contains the word folders (a01, a02, etc...)
-    
+
     dataset = []
 
     with open(words_file) as f:
         # line_counter counts the relevant lines to get the desired batch size
         line_counter = 0
         Y = []
-        X = []
+        X = torch.empty((batch_size, 1, image_size[0], image_size[1]))
         for line in f:
             if line_counter < batch_size:
                 # skip empty lines and information at the beginning
@@ -128,38 +137,50 @@ def data_loader(words_file, data_dir, batch_size):
                 file_name_split = line_split[0].split('-')
                 file_name = '/' + file_name_split[0] + '/' + file_name_split[0] + '-' + file_name_split[1] + '/' + line_split[0] + '.png'
                 # load image, convert to greyscale and then to torch tensor
-                img = PImage.open(data_dir + file_name).convert('L')
+                try:
+                    img = cv2.imread(data_dir + file_name)
+                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                except:
+                    #skip broken images
+                    continue
+                img = cv2.resize(gray, (image_size[1], image_size[0]))
                 converter = torchvision.transforms.ToTensor()
                 x = converter(img)
                 # append the image and the target, obtained from the corresponding words.txt line, to the X,Y lists
-                X.append(torch.squeeze(x))
+                X[line_counter, 0] = torch.squeeze(x)
                 y = line_split[-1]
                 Y.append(y)
                 line_counter += 1
             else:
-                # add the lists to the dataset and reset the lists and line_counter variable 
+                # add the lists to the dataset and reset the lists and line_counter variable
                 data = (X, Y)
                 dataset.append(data)
                 Y = []
-                X = []
+                X = torch.empty((batch_size, 1, image_size[0], image_size[1]))
                 line_counter = 0
+                if (len(dataset)%10 == 0):
+                    print("loaded batch {}/{}".format(len(dataset), math.ceil(115338/batch_size)))
+                    print("quitting early for testing")
+                    return dataset
         # if total number of lines is not divisible by the batch_size, the remaining smaller batch must be added at the end
         if len(Y) != 0 and X != 0:
-            data = (X,Y)
+            short_X = torch.empty((len(Y), 1, image_size[0], image_size[1]))
+            short_X[:len(Y),:,:,:] = X[:len(Y),:,:,:]
+            data = (short_X,Y)
             dataset.append(data)
-        
-    
+
+
     return dataset
-    
-    
+
+
 
 
 if __name__=="__main__":
-    model = Net()
+    model = Net().to(device)
     n_epochs = 10
     #TODO: Define Dataloader properly (either via pytorch or the github example)
-    dataloader = None
+    dataloader = data_loader("../dataset/words.txt", "../dataset/images", 50, (32, 128))
+    print(len(dataloader))
     for epoch in range(n_epochs):
         print("Training Epoch "+ str(epoch+1))
-        #training(model, dataloader)
-        print("Not training due to missing dataloader implementation")
+        training(model, dataloader)
